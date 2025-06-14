a6f9e1c5312a7ee2996881f1021e182482beab8f
shenmadu <tashen.madu@gmail.com>
2025-06-14 10:50:16 +0530

    feat : book seeder

--- Backend/book-rental-backend/database/seeders/BookSeeder.php
+++ Backend/book-rental-backend/database/seeders/BookSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Book;
 
 class BookSeeder extends Seeder
 {
@@ -12,6 +13,41 @@ class BookSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $books = [
+            [
+                'title' => 'The Great Gatsby',
+                'author' => 'F. Scott Fitzgerald',
+                'published_date' => '1925-04-10',
+                'is_available' => true,
+            ],
+            [
+                'title' => '1984',
+                'author' => 'George Orwell',
+                'published_date' => '1949-06-08',
+                'is_available' => true,
+            ],
+            [
+                'title' => 'To Kill a Mockingbird',
+                'author' => 'Harper Lee',
+                'published_date' => '1960-07-11',
+                'is_available' => false,
+            ],
+            [
+                'title' => 'Brave New World',
+                'author' => 'Aldous Huxley',
+                'published_date' => '1932-08-31',
+                'is_available' => true,
+            ],
+            [
+                'title' => 'Moby-Dick',
+                'author' => 'Herman Melville',
+                'published_date' => '1851-10-18',
+                'is_available' => false,
+            ],
+        ];
+
+        foreach ($books as $book) {
+            Book::create($book);
+        }
     }
 }
